57558c73b4d330d7399f4cd926de9b3a57a8f7a4
GameDevLifeKJ <165800171+GameDevLifeKJ@users.noreply.github.com>
2025-05-10 06:50:56 +0000

    88페이지

---
+++ c++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+inline void keep_window_open() {char ch >>ch;}
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cin.get();
+    return 0;
+}
